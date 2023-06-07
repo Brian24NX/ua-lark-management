aad972ae2a57f2fd117925847c663ee56ed63b5c
majun@junoxi.com <114054289+junmat2022@users.noreply.github.com>
2023-06-07 10:49:24 +0800

    框架尺寸优化

--- ruoyi-ui/src/views/system/user/index.vue
+++ ruoyi-ui/src/views/system/user/index.vue
@@ -208,8 +208,8 @@
     />
 
     <!-- 添加或修改用户配置对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="600px" append-to-body>
-      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+    <el-dialog :title="title" :visible.sync="open" width="650px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="120px">
         <el-row>
           <el-col :span="12">
             <el-form-item :label="$t('nickName')" prop="nickName">
